4392392d82fe1c3ac5c8e7462542a6366ab23535
cartersw <carterwildenradt@gmail.com>
2026-07-25 00:07:46 -0400

    validation added in hotel + country classes

--- HotelListing.Api/Data/Hotel.cs
+++ HotelListing.Api/Data/Hotel.cs
@@ -1,12 +1,18 @@
-﻿namespace HotelListing.Api.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Api.Data;
 
 public class Hotel
 {
     public int Id { get; set; }
-    public string Name { get; set; } 
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    [MaxLength(10)]
     public string Address { get; set; }
+    [Range(1.0, 5.0)]
     public double Rating { get; set; }
+    [Required]
     public int CountryId { get; set; }
     public Country? Country { get; set; }
-
 }
