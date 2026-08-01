3ae1bdf6bb7b88197e9457219cd81fd3ff27ff90
cartersw <carterwildenradt@gmail.com>
2026-08-01 02:45:55 -0400

    authentication defaults

---
+++ HotelListing.Api/Constants/AuthenticationDefaults.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelListing.Api.Constants
+{
+    public class AuthenticationDefaults
+    {
+        public const string ApiKeyScheme = "ApiKey";
+        public const string ApiKeyHeaderName = "X-Api-Key";
+        public const string AppName = "HotelListingApi";
+    }
+}
