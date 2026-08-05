d1633d623468584a738870ffbca1937addbbdb61
cartersw <carterwildenradt@gmail.com>
2026-08-05 05:42:53 -0400

    booking table

---
+++ HotelListing.Api/Data/Booking.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Api.Data
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+        public ApplicationUser User { get; set; }
+
+        public DateOnly CheckIn { get; set; }
+
+        public DateOnly Checkout { get; set; }  
+
+        public int Guests { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAtUtc { get; set; }
+
+        public string Status = "Pending, Confirmed, Cancelled";
+    }
+   }
+
