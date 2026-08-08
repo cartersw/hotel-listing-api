6d31a0f16392c3e3206931541b594b840a458df5
cartersw <carterwildenradt@gmail.com>
2026-08-08 02:49:50 -0400

    Create HotelAdmin Authorization -ManageHotelRequirement -ManageHotelAuthorizationHandler

---
+++ HotelListing.Api/Authorization/Handlers/ManageHotelAuthorizationHandler.cs
@@ -0,0 +1,44 @@
+﻿using HotelListing.Api.Authorization.Requirements;
+using HotelListing.Api.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace HotelListing.Api.Authorization.Handlers
+{
+    public class ManageHotelAuthorizationHandler(HotelListingDbContext dbContext) 
+        : AuthorizationHandler<ManageHotelRequirement, Hotel>
+    {
+      
+        protected override async Task HandleRequirementAsync(
+            AuthorizationHandlerContext authContext, 
+            ManageHotelRequirement requirement, 
+            Hotel resource)
+        {
+            if (authContext.User.IsInRole("Administrator"))
+            {
+                authContext.Succeed(requirement);
+                return;
+            }
+
+            var userId = authContext.User
+                .FindFirst(ClaimTypes.NameIdentifier)?
+                .Value;
+
+            if(userId == null)
+            {
+                return;
+            }
+
+            var managesHotel = await dbContext.HotelAdmins.AnyAsync(ha =>
+            ha.UserId == userId && ha.HotelId == resource.Id);
+
+            if (managesHotel)
+            {
+                authContext.Succeed(requirement);
+            }
+
+
+        }
+    }
+}
---
+++ HotelListing.Api/Authorization/Requirements/ManageHotelRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace HotelListing.Api.Authorization.Requirements
+{
+    public class ManageHotelRequirement : IAuthorizationRequirement
+    {
+    }
+}
