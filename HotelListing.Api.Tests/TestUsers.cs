b2dd739158a4515f65546438321faf8e95ef6a0c
cartersw <carterwildenradt@gmail.com>
2026-08-11 03:51:53 -0400

    Testing constants

--- HotelListing.Api.Tests/TestUsers.cs
+++ HotelListing.Api.Tests/TestUsers.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace HotelListing.Api.Tests
 {
-    internal class TestUsers
+    public static class TestUsers
     {
+        public const string AdminEmail = "admin@test.com";
+        public const string ManagerOneEmail = "manager1@test.com";
+        public const string ManagerTwoEmail = "manager2@test.com";
+        public const string UserEmail = "user@test.com";
+
+
+        public const string Password = "TEST_PASSWORD";
+
+    }
+
+
+    public static class TestHotels
+    {
+        public const int HotelOneId = 1001;
+        public const int HotelTwoId = 1002;
+    }
+
+
+    public static class TestCountries
+    {
+        public const int CountryOneId = 1001;
     }
 }
