77f01e0c4fbcfd85f84498e8534ba614ec32481e
cartersw <carterwildenradt@gmail.com>
2026-08-13 21:38:03 -0400

    Role configuration uses constants

--- HotelListing.Api/Data/Configurations/RoleConfiguration.cs
+++ HotelListing.Api/Data/Configurations/RoleConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HotelListing.Api.Common.Constants;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -12,22 +13,22 @@ namespace HotelListing.Api.Data.Configurations
                 new IdentityRole
                 {
                     Id = "ef3013de-7dad-4310-813b-d7d4486874db",
-                    Name = "Administrator",
-                    NormalizedName = "ADMINISTRATOR",
+                    Name = RoleNames.Administrator,
+                    NormalizedName = RoleNames.Administrator.ToUpper(),
                     ConcurrencyStamp = "ef3013de-7dad-4310-813b-d7d4486874db"
                 },
                 new IdentityRole
                 {
                     Id = "8d139698-a775-4684-abf8-2765e9bd24ce",
-                    Name = "User",
-                    NormalizedName = "USER",
+                    Name = RoleNames.User,
+                    NormalizedName = RoleNames.User.ToUpper(),
                     ConcurrencyStamp = "8d139698-a775-4684-abf8-2765e9bd24ce"
                 },
                 new IdentityRole
                 {
                     Id = "f823e597-8558-4810-b6e6-570c4c0fcac7",
-                    Name = "Hotel Admin",
-                    NormalizedName = "HOTEL ADMIN",
+                    Name = RoleNames.HotelAdmin,
+                    NormalizedName = RoleNames.HotelAdmin.ToUpper(),
                     ConcurrencyStamp = "f823e597-8558-4810-b6e6-570c4c0fcac7"
                 }
                 );
