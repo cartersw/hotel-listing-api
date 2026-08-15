3834ce91afedfdbfd48c3cc3906f76f971b1c8cc
cartersw <carterwildenradt@gmail.com>
2026-08-15 03:32:59 -0400

    Indexing on country name and shortname

---
+++ HotelListing.Api.Domain/Configurations/CountryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelListing.Api.Domain.Configurations
+{
+    internal class CountryConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.HasIndex(x => x.Name);
+            builder.HasIndex(x => x.ShortName);
+        }
+    }
+}
---
+++ HotelListing.Api.Domain/HotelListing/Api/Migrations/20260815072932_AddIndexingCountry.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexingCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortName",
+                table: "Countries",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_Name",
+                table: "Countries",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_ShortName",
+                table: "Countries",
+                column: "ShortName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_Name",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_ShortName",
+                table: "Countries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortName",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Countries",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
