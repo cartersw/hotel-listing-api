b0eb55274722a2b46aaec2c9a00bd5434189c741
cartersw <carterwildenradt@gmail.com>
2026-08-14 21:11:17 -0400

    BaseFilterParameters model

---
+++ HotelListing.Api.Common/Models/Filtering/BaseFilterParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelListing.Api.Common.Models.Filtering
+{
+    public abstract class BaseFilterParameters
+    {
+        public string? Search { get; set; }
+        public string? SortBy { get; set; }
+        public bool SortDescending { get; set; } = false;
+    }
+}
